ae540831ac9aa74e9c432151e841004ced5933d0
sachin-subramanian <43053781+sachin-subramanian@users.noreply.github.com>
2020-12-05 15:17:58 -0700

    added changes

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => {
+    return (
+        <Layout>
+            <SEO title ="About" />
+            <h1>About</h1>
+            <p>My name is Sachin and this page is about me!</p>
+        </Layout>
+    )
+}
+
+export default About
