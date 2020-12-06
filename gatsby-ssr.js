dacad613dbe650f2e0bf0006fb98daea32d20ca0
sachin-subramanian <43053781+sachin-subramanian@users.noreply.github.com>
2020-12-05 19:53:14 -0700

    adding auth

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,17 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from "react"
+import { Auth0Provider } from "@auth0/auth0-react"
+
+export const wrapRootElement = ({element}) => {
+    return (
+        <Auth0Provider
+        domain= "dev-8ennyzey.us.auth0.com"
+        clientId= "d0wI2qRgtAd0yEInMwTvU4HH5HEwlurn"
+        redirectUri= "http://localhost:8000/blog"
+        >
+            {element}
+        </Auth0Provider>
+    )
+}
